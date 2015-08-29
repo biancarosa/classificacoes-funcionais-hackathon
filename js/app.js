44d4522b772e3699c09ca08148cf60f924fcebd7
biancarosa <me@biancarosa.com.br>
2015-08-29 10:59:27 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', [
+  'myApp.controllers', 'ngRoute'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'partials/principal.html',
+      controller: 'MainController'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+    
+});
---
+++ js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MainController', function ($scope) {
+    // write Ctrl here
+    console.log('hi');
+
+ });
