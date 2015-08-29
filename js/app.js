927dc6ac912a8787a90e67975ae7a166b4b86252
biancarosa <me@biancarosa.com.br>
2015-08-29 14:51:21 -0300

    Um monte de  coisa

--- js/app.js
+++ js/app.js
@@ -8,8 +8,19 @@ angular.module('myApp', [
 config(function ($routeProvider, $locationProvider) {
   $routeProvider.
     when('/', {
-      templateUrl: 'partials/principal.html',
-      controller: 'MainController'
+      templateUrl: 'partials/principal.html'
+    }).
+    when('/:sport/:disability', {
+      templateUrl: 'partials/detalhe.html',
+      controller: 'DetailController'
+    }).
+    when('/faq', {
+      templateUrl: 'partials/faq.html',
+      controller: 'FaqController'
+    }).
+    when('/quem_pode', {
+      templateUrl: 'partials/quem_pode.html',
+      controller: 'FaqController'
     }).
     otherwise({
       redirectTo: '/'
--- js/controllers.js
+++ js/controllers.js
@@ -2,8 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MainController', function ($scope) {
+
+var sport = {
+	'atletismo' : 'Atletismo'
+};
 
 
-  });
+var disability = {
+	'visual' : 'Deficiência Visual',
+	'intelectual' : 'Deficiência Intelectual',
+	'motora' : 'Deficiência Motora',
+	'ausencia_ou_comprometimento' : 'Ausência ou comprometimento de membros',
+	'nanismo' : 'Nanismo'
+}
+
+angular.module('myApp.controllers', [])
+ 	.controller('DetailController', function ($scope, $routeParams) {
+ 		$scope.sport = sport[$routeParams.sport];
+ 		$scope.disability = disability[$routeParams.disability];
+
+ 		$scope.routeSport = $routeParams.sport;
+ 		$scope.routeDisability = $routeParams.disability;
+
+		$(".navbar-collapse").collapse('hide');
+ 	})
+ 	.controller('FaqController', function ($scope) {
+		$(".navbar-collapse").collapse('hide');
+ 	});
